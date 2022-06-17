68a8e44e573f606f890649a0fe7540f94eb0e414
kovalenkoandri <58831202+kovalenkoandri@users.noreply.github.com>
2022-06-17 16:34:45 +0300

    task 02 done (#2)

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,29 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+// В HTML есть пустой список ul#ingredients
+// Напиши скрипт, который для каждого элемента массива ingredients:
+
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul.ingredients
+const potatoes = document.createElement('li');
+potatoes.textContent = ingredients[0];
+potatoes.classList.add('item');
+const mushrooms = document.createElement('li');
+mushrooms.textContent = ingredients[1];
+mushrooms.classList.add('item');
+const garlic = document.createElement('li');
+garlic.textContent = ingredients[2];
+garlic.classList.add('item');
+const tomatos = document.createElement('li');
+tomatos.textContent = ingredients[3];
+tomatos.classList.add('item');
+const herbs = document.createElement('li');
+herbs.textContent = ingredients[4];
+herbs.classList.add('item');
+const condiments = document.createElement('li');
+condiments.textContent = ingredients[5];
+condiments.classList.add('item');
+document.getElementById('ingredients').append(potatoes, mushrooms, garlic, tomatos, herbs, condiments);
